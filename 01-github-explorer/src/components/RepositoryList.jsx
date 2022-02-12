cdcdd4c9f0d8cf99350e15378c693df8adbef3ef
Simão Neto <simaomenezesneto@gmail.com>
2022-02-12 10:27:37 +0000

    feat: My first component

---
+++ 01-github-explorer/src/components/RepositoryList.jsx
@@ -0,0 +1,16 @@
+export function RepositoryList() {
+    return (
+        <section className="repository-list">
+            <h1>Repositoty of List</h1>
+            <ul>
+                <li>
+                    <strong>unform</strong>
+                    <p>Forms in React</p>
+                    <a href="">
+                        Repository Acess
+                    </a>
+                </li>
+            </ul>
+        </section>
+    );
+}
