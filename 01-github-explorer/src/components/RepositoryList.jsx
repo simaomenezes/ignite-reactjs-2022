dd743ef2f2ec234ef330c5e35cc44044714cae45
Simão Neto <simaomenezesneto@gmail.com>
2022-02-12 11:53:00 +0000

    feat: call http - api github, working with useEffect

--- 01-github-explorer/src/components/RepositoryList.jsx
+++ 01-github-explorer/src/components/RepositoryList.jsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import { RepositoryItem } from "./RepositoryItem";
 import '../styles/repositories.scss';
 const repository = {
@@ -6,8 +7,14 @@ const repository = {
     link: 'https://github.com/unform/unform',
     descLink: 'LInk para github'
 }
-
+// https://api.github.com/orgs/rocketseat/repos
 export function RepositoryList() {
+    const [repositories, setRepositories] = useState([]);
+    useEffect(() => {
+        fetch('https://api.github.com/orgs/rocketseat/repos')
+        .then(response => response.json())
+        .then(data => setRepositories(data))
+    }, []);
     return (
         <section className="repository-list">
             <h1>Repositoty of List</h1>
