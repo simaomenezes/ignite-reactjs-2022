4195040eb08e406f13e3c47112d83f39f4410c78
Simão Neto <simaomenezesneto@gmail.com>
2022-02-21 22:30:00 +0000

    feat: Setting modal of create transaction

--- snmoney/src/App.tsx
+++ snmoney/src/App.tsx
@@ -1,7 +1,8 @@
-import { useState } from "react";
 import Modal from 'react-modal';
+import { useState } from "react";
 import { Dashboard } from "./components/Dashboard";
 import { Header } from "./components/Header";
+import { NewTransactionModal } from "./components/NewTransactionModal";
 import { GlobalStyle } from "./styles/global";
 
 Modal.setAppElement("#root");
@@ -16,13 +17,8 @@ export function App() {
   return (
     <>
       <Header onOpenNewTransactionModal={handleOpenNewTransactionModal}/>
-      <Dashboard/>
-      <Modal 
-        isOpen={isNewTransactionModalOpen} 
-        onRequestClose={handleCloseNewTransactionModal}
-      >
-        <h2>Cadastrar Transação</h2>
-      </Modal>
+      <Dashboard/>   
+      <NewTransactionModal isOpen={isNewTransactionModalOpen} onRequestClose={handleCloseNewTransactionModal}/>  
       <GlobalStyle />
     </>
   );
