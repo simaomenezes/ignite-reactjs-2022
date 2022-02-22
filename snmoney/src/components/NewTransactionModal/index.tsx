40892a031ceeccc0d292f07b7b0bfdb7f24fe345
Simão Neto <simaomenezesneto@gmail.com>
2022-02-22 22:55:01 +0000

    feat: Create button of type

--- snmoney/src/components/NewTransactionModal/index.tsx
+++ snmoney/src/components/NewTransactionModal/index.tsx
@@ -1,6 +1,8 @@
 import Modal from 'react-modal';
 import closeImg from '../../assets/close.svg';
-import { Container } from './styles';
+import outcomeImg from '../../assets/outcome.svg';
+import incomeImg from '../../assets/income.svg';
+import { Container, TransactionTypeContainer } from './styles';
 
 interface NewTransactionModalProps {
     isOpen: boolean,
@@ -30,6 +32,20 @@ export function NewTransactionModal({ isOpen, onRequestClose }: NewTransactionMo
                     <input type="number"
                         placeholder='Valor'
                     />
+                    <TransactionTypeContainer>
+                        <button 
+                            type='button'
+                        >
+                            <img src={incomeImg} alt="Entrada" />
+                            <span>Entrada</span>
+                        </button>
+                        <button 
+                            type='button'
+                        >
+                            <img src={outcomeImg} alt="Saída" />
+                            <span>Saída</span>
+                        </button>
+                    </TransactionTypeContainer>
                     <input
                         placeholder='Categoria'
                     />
