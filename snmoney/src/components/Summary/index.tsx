e8f5f38e369de75d5c35bd603577cf419bd61c2c
Simão Neto <simaomenezesneto@gmail.com>
2022-02-24 22:52:32 +0000

    refactor: Format price

--- snmoney/src/components/Summary/index.tsx
+++ snmoney/src/components/Summary/index.tsx
@@ -7,7 +7,20 @@ import { Container } from "./styles";
 export function Summary() {
     const { transactions } = useContext(TransactionsContext);
 
-    console.log(transactions);
+    const summary = transactions.reduce((acc, transaction) => {
+        if(transaction.type === 'deposit'){
+            acc.deposits += transaction.amount;
+            acc.total += transaction.amount;
+        } else {
+            acc.withdraws += transaction.amount;
+            acc.total -= transaction.amount;
+        }
+        return acc;
+    }, {
+        deposits:0,
+        withdraws:0,
+        total:0,
+    })
     return (
         <Container>
             <div>
@@ -16,7 +29,12 @@ export function Summary() {
                     <img src={incomeImg} alt="Entradas" />
                 </header>
                 <strong>
-                    1000,00
+                    {
+                        new Intl.NumberFormat('pt-BR', {
+                            style: 'currency',
+                            currency: 'BRL'
+                        }).format(summary.deposits)
+                    }
                 </strong>
             </div>
             <div>
@@ -25,7 +43,12 @@ export function Summary() {
                     <img src={outcomeImg} alt="Saídas" />
                 </header>
                 <strong>
-                    -500,00
+                    -{
+                        new Intl.NumberFormat('pt-BR', {
+                            style: 'currency',
+                            currency: 'BRL'
+                        }).format(summary.withdraws)
+                    }
                 </strong>
             </div>
             <div className='highligh-background'>
@@ -34,7 +57,12 @@ export function Summary() {
                     <img src={totalImg} alt="Total" />
                 </header>
                 <strong>
-                    500,00
+                    {
+                        new Intl.NumberFormat('pt-BR', {
+                            style: 'currency',
+                            currency: 'BRL'
+                        }).format(summary.total)
+                    }
                 </strong>
             </div>
         </Container>
