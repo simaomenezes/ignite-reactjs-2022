7c14a04790e190ee2256c249a913a0d9b2a64d06
Simão Neto <simaomenezesneto@gmail.com>
2022-02-24 22:34:06 +0000

    refact: Finish transactions

--- snmoney/src/contexts/TransactionsContext.tsx
+++ snmoney/src/contexts/TransactionsContext.tsx
@@ -16,7 +16,7 @@ interface TransactionProviderProps {
 }
 interface TransactionContextData {
     transactions: Transaction[];
-    createTransaction: (transaction: TransactionInput) => void;
+    createTransaction: (transaction: TransactionInput) => Promise<void>;
 }
 export const TransactionsContext = createContext<TransactionContextData>(
     {} as TransactionContextData
@@ -29,8 +29,13 @@ export function TransactionsProvider({children}: TransactionProviderProps) {
             .then(response => setTransactions(response.data.transactions))
     }, []);
 
-    function createTransaction(transaction: TransactionInput) {
-        api.post('/transactions', transaction);
+    async function createTransaction(transactionInput: TransactionInput) {
+        const response = await api.post('/transactions', {...transactionInput, createdAt: new Date()});
+        const { transaction } = response.data;
+        setTransactions([
+            ...transactions,
+            transaction
+        ]);
     }
     return (
         <TransactionsContext.Provider value={{ transactions, createTransaction }}>
